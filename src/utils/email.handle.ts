7d982382f532c9b2025902c292308aa68fd2d9a9
Itziar Mensa <itziar.mensa@estudiantat.upc.edu>
2023-12-17 17:21:04 +0100

    isEmail validator changed to flightlinebcn.com

--- src/utils/email.handle.ts
+++ src/utils/email.handle.ts
@@ -1,6 +1,6 @@
 export function isEmail(input: string): any {
     if (input !== null) {
-        const pattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
+        const pattern = /\b[A-Za-z0-9._%+-]+@flightlinebcn\.com\b/;
         return pattern.test(input);
     } 
 }
