a193a0e260f8bbd1f3ed5532f6242e9ece0c3f0f
Itziar <itziar.mensa08@gmail.com>
2024-07-30 11:10:34 +0200

    v0.1.0

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { connect } from "mongoose";
+import { connect, ConnectOptions } from "mongoose";
 
 export async function dbConnect(): Promise<void> {
     const DB_URI = process.env.DB_URI || 'mongodb://mongo:27017/tfg-backend';
@@ -8,8 +8,11 @@ export async function dbConnect(): Promise<void> {
         console.error("DB_URI is not defined");
         process.exit(1);
     }
+    const options: ConnectOptions = {
+        serverSelectionTimeoutMS: 30000,
+    };
     try {
-        await connect(DB_URI);
+        await connect(DB_URI, options);
         console.log("Connection Ready");
     } catch (error) {
         console.error("Connection Error:", error);
